15687278e038faf07fbb1fc32a7b6472f1a1b776
857689975 <857689975@qq.com>
2018-11-16 11:05:57 +0800

    模板完成

---
+++ public/javascripts/public.js
@@ -0,0 +1,37 @@
+includeScript('../public/javascripts/jquery.form.min.js');
+includeScript('../public/javascripts/scripts.js');
+includeScript('../public/javascripts/jquery.blockUI.min.js');
+includeScript('../public/javascripts/woocommerce.min.js');
+includeScript('../public/javascripts/jquery.cookie.min.js');
+includeScript('../public/javascripts/cart-fragments.min.js');
+includeScript('../public/javascripts/core.min.js');
+includeScript('../public/javascripts/widget.min.js');
+includeScript('../public/javascripts/tabs.min.js');
+includeScript('../public/javascripts/accordion.min.js');
+includeScript('../public/javascripts/mouse.min.js');
+includeScript('../public/javascripts/slider.min.js');
+includeScript('../public/javascripts/mediaelement-and-player.min.js');
+includeScript('../public/javascripts/wp-mediaelement.js');
+includeScript('../public/javascripts/third-party.min.js');
+includeScript('../public/javascripts/isotope.pkgd.min.js');
+includeScript('../public/javascripts/js.js');
+includeScript('../public/javascripts/modules.min.js');
+includeScript('../public/javascripts/blog.min.js');
+includeScript('../public/javascripts/comment-reply.min.js');
+includeScript('../public/javascripts/js_composer_front.min.js');
+includeScript('../public/javascripts/like.js');
+includeScript('../public/javascripts/wp-embed.min.js');
+
+
+
+
+
+
+
+
+
+
+function includeScript(url) {
+	document.write('<script type="text/javascript" src="' + url + '"></script>');
+	return false;
+}
